a4b2ff019d28723a93c4dce8a07e7e282f70b5bc
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-11-20 11:58:12 -0500

    Initial Commit

---
+++ motivation Watch App/motivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivationApp.swift
+//  motivation Watch App
+//
+//  Created by Bryan de Bourbon on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct motivation_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ motivation/motivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivationApp.swift
+//  motivation
+//
+//  Created by Bryan de Bourbon on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct motivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
