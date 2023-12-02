09c1f9d9e6c25744cc81ee7d9efda58856d3d9d5
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-12-02 16:44:11 -0500

    testing

--- SharedUserDefaults.swift
+++ SharedUserDefaults.swift
@@ -12,7 +12,7 @@ class SharedUserDefaults {
 
   func addContributionDays(completion: @escaping () -> Void) {
     SharedUserDefaults.fetcher.fetchGitHubData(
-      accessToken: "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"
+      accessToken: ""
     ) { result in
       switch result {
       case .success(let response):
@@ -22,11 +22,11 @@ class SharedUserDefaults {
           .flatMap { $0.contributionDays }
 
         // Save the updated array
-          if let encodedData = try? JSONEncoder().encode(newContributionDays) {
-            self.userDefaults?.set(encodedData, forKey: "sharedArray")
-            WidgetCenter.shared.reloadAllTimelines()
-          }
-          completion()
+        if let encodedData = try? JSONEncoder().encode(newContributionDays) {
+          self.userDefaults?.set(encodedData, forKey: "sharedArray")
+          WidgetCenter.shared.reloadAllTimelines()
+        }
+        completion()
       case .failure(let error):
         print("Error fetching data: \(error)")
       }
@@ -34,6 +34,8 @@ class SharedUserDefaults {
   }
 
   func getSharedArray() -> [ContributionDay] {
+    print("in get Array")
+    
     guard let data = userDefaults?.data(forKey: "sharedArray"),
       let contributionDays = try? JSONDecoder().decode([ContributionDay].self, from: data)
     else {
